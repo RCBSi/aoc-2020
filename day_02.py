fe98e2314364880c15dc48a07b99891f04e8a75a
Jesse Vuorinen <jesse.vuorinen@outlook.com>
2020-12-02 09:18:49 +0200

    second day

---
+++ day_02.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+from common import read_input
+
+
+def get_cleaned_input():
+    res = [x for x in read_input("data/day_02.txt")]
+    return res
+
+
+def parse_line(line):
+    policy, pw = line.split(": ")
+    nums_raw, letter = policy.split(" ")
+    num_low, num_high = [int(i) for i in nums_raw.split("-")]
+    return num_low, num_high, letter, pw
+
+
+def validate_simple(line):
+    num_low, num_high, letter, pw = parse_line(line)
+
+    is_valid = num_low <= pw.count(letter) <= num_high
+    return is_valid
+
+
+def validate_complex(line):
+    num_low, num_high, letter, pw = parse_line(line)
+
+    first_equals = pw[num_low - 1] == letter
+    second_equals = pw[num_high - 1] == letter
+
+    is_valid = sum([first_equals, second_equals]) == 1
+    return is_valid
+
+
+def sum_of_valids(password_list, func):
+    return sum(func(x) for x in password_list)
+
+
+if __name__ == "__main__":
+    password_list = get_cleaned_input()
+
+    # Part 1
+    answer_1 = sum_of_valids(password_list, validate_simple)
+    print(f"Part 1 answer: {answer_1}")
+
+    # Part 2
+    answer_2 = sum_of_valids(password_list, validate_complex)
+    print(f"Part 2 answer: {answer_2}")
---
+++ template.py
@@ -0,0 +1,21 @@
+from common import read_input
+
+def get_cleaned_input():
+    res = [x for x in read_input("data/day_xx.txt")]
+    return res
+
+
+def solve():
+    return 1
+
+
+if __name__ == "__main__":
+    inputs = get_cleaned_input()
+
+    # Part 1
+    # answer_1 = solve(inputs)
+    # print(f"Part 1 answer: {answer_1}")
+
+    # Part 2
+    # answer_2 = solve(inputs)
+    # print(f"Part 2 answer: {answer_2}")
