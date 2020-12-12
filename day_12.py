82b35fd8a5ad63b5b78f498859896aa6c3af219b
Jesse Vuorinen <jesse.vuorinen@outlook.com>
2020-12-12 20:11:11 +0200

    day 12 complex numbers

--- day_12.py
+++ day_12.py
@@ -1,100 +1,41 @@
 from common import read_input
-import numpy as np
-
-from itertools import cycle
-
-def get_cleaned_input():
-    res = [x for x in read_input("data/day_12.txt")]
-    return res
-
-def _get_next_hdg(hdg):
-    pass
-
-def parse_action(s):
-    return s[0], int(s[1:])
 
 DIRS = {
-    'N': np.array([0, 1]),
-    'E': np.array([1, 0]),
-    'S': np.array([0, -1]),
-    'W': np.array([-1, 0]),
+    "N": complex(0, 1),
+    "E": complex(1, 0),
+    "S": complex(0, -1),
+    "W": complex(-1, 0),
 }
 
-import math
+ROTS = {"R": -1, "L": 1}
 
-def _rotate_wpt(point, cmd, value):
-    v = int(value/90)
-    if cmd == 'R':
-        v *= -1
-    cp = complex(*point)
-    new = (1j **v )* cp
-    return np.array((round(new.real), round(new.imag)))
 
-def _get_next_hdg(hdg, cmd, value):
-    "".join(DIRS.keys())
+def _rotate_point(point, cmd, value):
+    v = ROTS[cmd] * (value / 90)
+    return (1j ** v) * point
 
-    if cmd == 'R':
-        it = cycle("".join(DIRS.keys()))
-    else:
-        it = cycle("".join(DIRS.keys())[::-1])
 
-    for i in it:
-        if i == hdg:
-            for _ in range(int(value/90)):
-                v = next(it)
-            return v         
+def solve(actions, wpt, pos_moves):
+    pos = complex(0, 0)
 
-class SimpleShip:
-    def __init__(self):
-        self.pos = np.array([0,0])
-        self.hdg = 'E'
+    for a in actions:
+        cmd, value = a[0], int(a[1:])
 
-    def update(self, action):
-        cmd, value = parse_action(action)
-
-        if cmd in 'NSEW':
-            self.pos += value*DIRS[cmd]
+        if cmd in "NSEW":
+            if pos_moves:
+                pos += value * DIRS[cmd]
+            else:
+                wpt += value * DIRS[cmd]
         elif cmd in "LR":
-            self.hdg = _get_next_hdg(self.hdg, cmd, value)
-        elif cmd == 'F':
-            self.pos += value*DIRS[self.hdg]
+            wpt = _rotate_point(wpt, cmd, value)
+        elif cmd == "F":
+            pos += value * wpt
 
-
-class ComplexShip:
-    def __init__(self):
-        self.pos = np.array([0,0])
-        self.wpt = np.array([10, 1])
-
-    def update(self, action):
-        cmd, value = parse_action(action)
-
-        if cmd in 'NSEW':
-            self.wpt += value*DIRS[cmd]
-        elif cmd in "LR":
-            self.wpt = _rotate_wpt(self.wpt, cmd, value)
-        elif cmd == 'F':
-            self.pos += value*self.wpt
-
+    print(f"Manhattan distance: {int(abs(pos.real) + abs(pos.imag))}")
 
 
 if __name__ == "__main__":
-    inputs = get_cleaned_input()
+    actions = read_input("data/day_12.txt")
 
-
-    s = SimpleShip()
-
-    for a in inputs:
-        s.update(a)
-
-    
-    # Part 1
-    # answer_1 = solve(inputs)
-    print(f"Part 1 answer: {sum(abs(a) for a in s.pos)}")
-
-    # Part 2
-    c = ComplexShip()
-    for a in inputs:
-        c.update(a)   
-        
-    # answer_2 = solve(inputs)
-    print(f"Part 2 answer: {sum(abs(a) for a in c.pos)}")
+    solve(actions, wpt=complex(1, 0), pos_moves=True)
+    solve(actions, wpt=complex(10, 1), pos_moves=False)
