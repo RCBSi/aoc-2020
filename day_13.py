d48a87a72a79c2d248bea1400d661473b96b08ed
Jesse Vuorinen <jesse.vuorinen@outlook.com>
2020-12-13 23:03:56 +0200

    day 13 cleaned

--- day_13.py
+++ day_13.py
@@ -5,61 +5,40 @@ import numpy as np
 from common import read_input
 
 
-def solve_1(raw_in):
+def parse_input(raw_in):
     a, b = raw_in
-
     earliest = int(a)
-    bus_ids = [int(x) for x in b.split(',') if x != 'x']
+    buses = [(int(b), a) for a, b in enumerate(b.split(","), 0) if b != "x"]
+    return earliest, buses
 
-    wait_times = np.array([x - (earliest % x) for x in bus_ids])
 
+def _get_wait_time(ts, bus_id):
+    t = bus_id - ts % bus_id
+    return t if t < bus_id else 0
+
+
+def solve_1(earliest, buses):
+    bus_ids = [b[0] for b in buses]
+    wait_times = [_get_wait_time(earliest, b) for b in bus_ids]
     min_id = np.argmin(wait_times)
-
     return wait_times[min_id] * bus_ids[min_id]
 
 
-def get_wait_time(ts, bus_id):
-    return (bus_id - ts % bus_id)
-
-def solve_2(raw_in):
-    _, b = raw_in
-    buses = [(int(b), a) for a, b in enumerate(b.split(','), 0) if b != 'x']
-    buses[0] = (buses[0][0], buses[0][0])
-    buses = sorted(buses)
-
-    min_so_far = 0
-    step = 1 
+def solve_2(buses):
+    tstamp, step = 0, 1
     for bus_id, condition in buses:
-        # print(".................")
-        # print(f"bus_id {bus_id}, condition {condition}, min so far {min_so_far}")
-        
-        # a = (condition + min_so_far*step)/bus_id
-        # print(a)
-        for tryout in count(min_so_far, step):
-            # tryout = next(i2)
-
-            to_wait = get_wait_time(tryout, bus_id)
-            # print(f"Tryout:{tryout}, step {step}, time to wait {to
-            # _wait}")
-
-            # print(f"Trying {tryout} with bus_id {bus_id} and step {step} => next leaves in {to_wait}")
-            if (to_wait == condition) | (condition % bus_id == to_wait):
-                new_step = np.lcm(step, bus_id)
-                # print(f"Condition fulfilled, upgrading step from {step} to {new_step}")
-                step = new_step
-                min_so_far = tryout
-                break
-    # print(tryout)
-    
-    return tryout
-
+        while _get_wait_time(tstamp, bus_id) != (condition % bus_id):
+            tstamp += step
+        step = np.lcm(step, bus_id)
+    return tstamp
 
 
 if __name__ == "__main__":
     raw_in = read_input("data/day_13.txt")
+    earliest, buses = parse_input(raw_in)
 
-    answer_1 = solve_1(raw_in)
-    answer_2 = solve_2(raw_in)
+    answer_1 = solve_1(earliest, buses)
+    answer_2 = solve_2(buses)
 
     print(f"Part 1 answer: {answer_1}")
     print(f"Part 2 answer: {answer_2}")
