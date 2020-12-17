53566aebe1931d1044ee608163f67ba419910a75
Jesse Vuorinen <jesse.vuorinen@outlook.com>
2020-12-17 22:49:53 +0200

    clean up

--- day_17.py
+++ day_17.py
@@ -2,37 +2,17 @@ from common import read_input
 
 import numpy as np
 
-# TODO Clean up
-# 1) Store as a set instead
-# 2) Clean up
 
 def parse_coords(raw_in, ndim):
-    coords = {}
+    coords = set()
     for i, l in enumerate(raw_in):
         for j, c in enumerate(l):
             if c == "#":
                 if ndim == 3:
-                    coords[(i, j, 0)] = c
+                    coords.add((i, j, 0))
                 elif ndim == 4:
-                    coords[(i, j, 0, 0)] = c
+                    coords.add((i, j, 0, 0))
     return coords
-
-
-# def print_coords(coords, z_layer):
-#     layer = []
-#     for k in coords.keys():
-#         if k[2] == z_layer:
-#             layer.append((k[0], k[1]))
-#     a = np.array(layer)
-#     xmin, xmax = min(a[:, 0]), max(a[:, 0])
-#     ymin, ymax = min(a[:, 1]), max(a[:, 1])
-
-#     s = ""
-#     for x in range(xmin, xmax+1):
-#         for y in range(ymin, ymax+1):
-#             s += coords.get((x, y, z_layer), '.')
-#         s += "\n"
-#     print(s)
 
 
 def get_neighbors(loc, ndim):
@@ -54,7 +34,7 @@ def get_neighbors(loc, ndim):
             for d in (-1, 0, 1)
         )
 
-    return (c for c in candidates if c != loc)
+    return set(c for c in candidates if c != loc)
 
 
 def count_occupied(coords):
@@ -62,52 +42,41 @@ def count_occupied(coords):
 
 
 def update(coords, ndim):
-    new = {}
-    # Get list of nodes and their neighbors
-    old_keys = set(coords.keys())
-    neighbors = set.union(*(set(get_neighbors(x, ndim)) for x in old_keys))
+    new = set()
 
-    for loc in old_keys:
-        v = coords.get(loc, ".")
-        n_neighbors = sum(coords.get(n, ".") == "#" for n in get_neighbors(loc, ndim))
-        if v == "#":
-            if n_neighbors in (2, 3):
-                new[loc] = "#"
+    # Check existing ones first
+    for loc in coords:
+        n_neighbors = sum((n in coords) for n in get_neighbors(loc, ndim))
+        if n_neighbors in (2, 3):
+            new.add(loc)
         else:
             if n_neighbors == 3:
-                new[loc] = "#"
+                new.add(loc)
 
-    for loc in neighbors:
-        v = coords.get(loc, ".")
-        n_neighbors = sum(coords.get(n, ".") == "#" for n in get_neighbors(loc, ndim))
-
+    # Then check their neighbors
+    possible = set.union(*(get_neighbors(x, ndim) for x in coords))
+    for loc in possible:
+        n_neighbors = sum((n in coords) for n in get_neighbors(loc, ndim))
         if n_neighbors == 3:
-            new[loc] = "#"
+            new.add(loc)
 
     return new
 
 
-def solve(raw_in, ndim):
+def solve(raw_in, ndim, sim_length):
     coords = parse_coords(raw_in, ndim)
 
-    for _ in range(6):
+    for _ in range(sim_length):
         coords = update(coords, ndim)
 
     return count_occupied(coords)
 
 
-def solve_2(raw_in):
-    return False
-
-
 if __name__ == "__main__":
     raw_in = read_input("data/day_17.txt")
 
-    # coords = update(coords)
-    # print_coords(coords, 0)
-
-    answer_1 = solve(raw_in, ndim=3)
-    answer_2 = solve(raw_in, ndim=4)
+    answer_1 = solve(raw_in, ndim=3, sim_length=6)
+    answer_2 = solve(raw_in, ndim=4, sim_length=6)
 
     print(f"Part 1 answer: {answer_1}")
     print(f"Part 2 answer: {answer_2}")
