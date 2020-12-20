36d95d3fabd0801a053f15554c2e3396b994490f
Jesse Vuorinen <jesse.vuorinen@outlook.com>
2020-12-20 22:06:10 +0200

    the piecing together process kinds works

--- day_20.py
+++ day_20.py
@@ -1,62 +1,106 @@
 from functools import reduce
 from operator import mul
+from itertools import cycle
 
 import numpy as np
 
 from common import array_to_string, read_input, str_to_array
+
+OPPOSITE_HDG = {
+    'n': 's',
+    'e': 'w',
+    's': 'n',
+    'w': 'e'
+}
 
 
 class Tile:
     def __init__(self, id, array):
         self.id = id
         self.arr = array
-    
+
     def __repr__(self):
         return f"Tile {self.id}\n{array_to_string(self.arr)}"
 
     def rotate(self):
         self.arr = np.rot90(self.arr)
-    
+
     def flip(self):
         self.arr = np.flip(self.arr, axis=1)
 
+
     def get_edges(self):
-        e1 = "".join(map(chr, self.arr[0,:]))
-        e2 = "".join(map(chr, self.arr[-1,:]))
-        e3 = "".join(map(chr, self.arr[:, 0]))
-        e4 = "".join(map(chr, self.arr[:, -1]))
-        return (e1, e2, e3, e4)
+        return {
+            "n": "".join(map(chr, self.arr[0, :])),
+            "s": "".join(map(chr, self.arr[-1, :])),
+            "w": "".join(map(chr, self.arr[:, 0])),
+            "e": "".join(map(chr, self.arr[:, -1])),
+        }
 
 
-def create_tiles(raw_in):
-    tiles = []
+def create_tiles_dict(raw_in):
+    tiles_dict = {}
 
     for line in raw_in:
-        a, b = line.split('\n', maxsplit=1)
+        a, b = line.split("\n", maxsplit=1)
         a = int(a[5:-1])
         b = str_to_array(b.split())
-        tiles.append(Tile(a, b))
+        tiles_dict[a] = Tile(a, b)
 
-    return tiles 
+    return tiles_dict
+
+
+def find_match(tile, hdg, tiles_dict):
+    opp_hdg = OPPOSITE_HDG[hdg]
+    for other in tiles_dict.values():
+        if tile == other:
+            continue
+        for _ in range(2):
+            for _ in range(4):
+                if tile.get_edges()[hdg] == other.get_edges()[opp_hdg]:
+                    return other
+                other.rotate()
+            other.flip()
+    # print("Match not found!")
+            
+
+def piece_together(tiles_dict, corner_tiles):
+    hdg_iter = cycle(['e', 's', 'w', 'n'])
+    hdg = next(hdg_iter)
+    # Start from one corner and rotate it so that it is the NW one
+    tile = tiles_dict[corner_tiles[0]]
+    del(tiles_dict[tile.id])
+    
+    while tiles_dict:
+        nxt = find_match(tile, hdg, tiles_dict)
+        if nxt:
+            print(f"Found next tile {tile.id}")
+            tile = nxt
+            del(tiles_dict[tile.id])
+            
+        if not nxt:
+            print("Direction finished, changing dir")
+            hdg = next(hdg_iter)
 
 
 
-def solve_1(tiles):
-    t = tiles[0]
-
+def get_corner_tiles(tiles_dict):
+    # TODO Clean up
+    tiles = list(tiles_dict.values())
     corner_pieces = []
     for t in tiles:
         s = 0
-        for e in t.get_edges():
-            others = set.union(*(set(t2.get_edges()) for t2 in tiles if (t != t2)))
+        for e in t.get_edges().values():
+            others = set.union(
+                *(set(t2.get_edges().values()) for t2 in tiles if (t != t2))
+            )
             others_flipped = set(x[::-1] for x in others)
             if (e in others) or (e in others_flipped):
                 s += 1
         if s == 2:
             corner_pieces.append(t.id)
 
-    return reduce(mul, corner_pieces)
-
+    return corner_pieces
 
 
 def solve_2(raw_in):
@@ -65,10 +109,10 @@ def solve_2(raw_in):
 
 if __name__ == "__main__":
     raw_in = read_input("data/day_20.txt", split_delimiter="\n\n")
-    tiles = create_tiles(raw_in)
+    tiles_dict = create_tiles_dict(raw_in)
 
-    answer_1 = solve_1(tiles)
+    corner_tiles = get_corner_tiles(tiles_dict)
     answer_2 = solve_2(raw_in)
 
-    print(f"Part 1 answer: {answer_1}")
+    print(f"Part 1 answer: {reduce(mul, corner_tiles)}")
     print(f"Part 2 answer: {answer_2}")
