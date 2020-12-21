07d4e4729634c6211dd174a2bd3e76f69709698d
Jesse Vuorinen <jesse.vuorinen@outlook.com>
2020-12-21 23:26:42 +0200

    clean up a bit

--- day_20.py
+++ day_20.py
@@ -1,6 +1,6 @@
 from functools import reduce
-from operator import mul
 from itertools import cycle
+from operator import mul
 
 import numpy as np
 
@@ -44,31 +44,28 @@ def create_tiles_dict(raw_in):
     return tiles_dict
 
 
-def get_corner_tiles(tiles_dict):
-    # TODO Clean up
+def find_corner_tiles(tiles_dict):
+    # Corner pieces have exactly 2 sides that have a match in other tiles
     tiles = list(tiles_dict.values())
     corner_pieces = []
     for t in tiles:
-        s = 0
+        n_matches = 0
         for e in t.get_edges().values():
-            others = set.union(
+            edges_to_test = set.union(
                 *(set(t2.get_edges().values()) for t2 in tiles if (t != t2))
             )
-            others_flipped = set(x[::-1] for x in others)
-            if (e in others) or (e in others_flipped):
-                s += 1
-        if s == 2:
+            edges_to_test_flipped = set(x[::-1] for x in edges_to_test)
+            if (e in edges_to_test) or (e in edges_to_test_flipped):
+                n_matches += 1
+        if n_matches == 2:
             corner_pieces.append(t.id)
-
     return corner_pieces
 
 
-def find_match(tile, hdg, unsolved_tiles, tiles_dict):
+def _find_match(tile, hdg, unsolved_tiles, tiles_dict):
     opp_hdg = OPPOSITE_HDG[hdg]
     for tile_id in unsolved_tiles:
         other = tiles_dict[tile_id]
-        if tile == other:
-            print("We have a BIG BUG")
         for _ in range(2):
             for _ in range(4):
                 if tile.get_edges()[hdg] == other.get_edges()[opp_hdg]:
@@ -98,15 +95,13 @@ def piece_together(tiles_dict, corner_tiles):
     # Loop and build the solution dict
     while unsolved_tiles:
         solution[tuple(loc)] = tile
-        match = find_match(tile, hdg, unsolved_tiles, tiles_dict)
+        match = _find_match(tile, hdg, unsolved_tiles, tiles_dict)
         if match:
-            # print(f"Found next tile {match.id}")
             tile = match
             unsolved_tiles.remove(tile.id)
             loc += LOC_OFFSET[hdg]
 
         if not match:
-            # print("Direction finished, changing dir")
             hdg = next(hdg_iter)
     solution[tuple(loc)] = tile
 
@@ -130,30 +125,31 @@ def piece_together(tiles_dict, corner_tiles):
     return Tile("FINALE", final)
 
 
-def get_roughness(array):
-    serpent_ascii = [
+def _calculate_roughness(array):
+    monster_ascii = [
         "                  # ",
         "#    ##    ##    ###",
         " #  #  #  #  #  #   ",
     ]
-    serpent_filter = (str_to_array(serpent_ascii) == ord("#")).astype(int)
-    serpent_y_len, serpent_x_len = serpent_filter.shape
+    monster_filter = (str_to_array(monster_ascii) == ord("#")).astype(int)
+    m_len_y, m_len_x = monster_filter.shape
 
     hashes = (array == ord("#")).astype(int)
 
     solution = np.zeros_like(hashes)
     max_y, max_x = solution.shape
-    for row in range(0, max_y-serpent_y_len+1):
-        for col in range(0, max_x-serpent_x_len+1):
-            original = hashes[row:row+serpent_y_len, col:col+serpent_x_len]
-            filtered = original * serpent_filter
-            if np.array_equal(filtered, serpent_filter):
-                solution[row:row+serpent_y_len, col:col+serpent_x_len] += filtered
-    
+    for row in range(0, max_y - m_len_y + 1):
+        for col in range(0, max_x - m_len_x + 1):
+            original = hashes[row : row + m_len_y, col : col + m_len_x]
+            filtered = original * monster_filter
+            if np.array_equal(filtered, monster_filter):
+                solution[row : row + m_len_y, col : col + m_len_x] += filtered
+
     roughness = np.sum((solution == 0) * hashes)
     return roughness
 
-def solve_2(tiles_dict, corner_tiles):
+
+def determine_roughness(tiles_dict, corner_tiles):
     final = piece_together(tiles_dict, corner_tiles)
 
     roughnesses = []
@@ -161,7 +157,7 @@ def solve_2(tiles_dict, corner_tiles):
         final.flip()
         for _ in range(4):
             final.rotate()
-            r = get_roughness(final.arr)
+            r = _calculate_roughness(final.arr)
             roughnesses.append(r)
 
     return min(roughnesses)
@@ -171,8 +167,8 @@ if __name__ == "__main__":
     raw_in = read_input("data/day_20.txt", split_delimiter="\n\n")
     tiles_dict = create_tiles_dict(raw_in)
 
-    corner_tiles = get_corner_tiles(tiles_dict)
-    answer_2 = solve_2(tiles_dict, corner_tiles)
+    corner_tiles = find_corner_tiles(tiles_dict)
+    roughness = determine_roughness(tiles_dict, corner_tiles)
 
     print(f"Part 1 answer: {reduce(mul, corner_tiles)}")
-    print(f"Part 2 answer: {answer_2}")
+    print(f"Part 2 answer: {roughness}")
