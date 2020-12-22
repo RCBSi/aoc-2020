98f95f5a55434fd6d04830bbbf105cddf4438294
Jesse Vuorinen <jesse.vuorinen@outlook.com>
2020-12-22 19:08:00 +0200

    max flow matching solution

--- day_21.py
+++ day_21.py
@@ -1,5 +1,4 @@
-from collections import defaultdict
-from copy import deepcopy
+import networkx as nx
 
 from common import read_input
 
@@ -14,61 +13,43 @@ def parse_inputs(raw_in):
     return foods
 
 
-def _build_lookups(foods):
-    ingt_holders = defaultdict(set)
-    allg_holders = defaultdict(set)
+def max_flow_solve(foods):
+    G = nx.DiGraph()
 
-    for idx, f in enumerate(foods):
-        ingredients, allergens = f
-        for allg in allergens:
-            allg_holders[allg].add(idx)
-        for ingt in ingredients:
-            ingt_holders[ingt].add(idx)
+    tmp_m, tmp_n = zip(*foods)
+    all_i = set.union(*tmp_m)
+    all_a = set.union(*tmp_n)
+    allg_holders = {
+        allg: set(i for i, f in enumerate(foods) if allg in f[1]) for allg in all_a
+    }
 
-    return dict(ingt_holders), dict(allg_holders)
+    for i in all_i:
+        G.add_edge("source", i, capacity=1.0)
+        G.add_edge(i, "safe")
+    for a in all_a:
+        G.add_edge(a, "sink", capacity=1.0)
+        possible_i = set.intersection(*(foods[f][0] for f in allg_holders[a]))
+        for i in possible_i:
+            G.add_edge(i, a, capacity=1.0)
 
+    G.add_edge("safe", "sink", capacity=len(all_i) - len(all_a))
 
-def solve(foods):
-    unsolved = deepcopy(foods)
-    solution = {}
-
-    while any(len(x[1]) > 0 for x in unsolved):
-        updates = []
-        ingt_holders, allg_holders = _build_lookups(unsolved)
-
-        # Check if common ingredients
-        interesting_allgs = [a for a, hs in allg_holders.items() if len(hs) >= 2]
-        for allg in interesting_allgs:
-            holders = allg_holders[allg]
-            common_ingts = set.intersection(*(unsolved[f][0] for f in holders))
-            common_allgs = set.intersection(*(unsolved[f][1] for f in holders))
-            if len(common_allgs) == len(common_ingts) == 1:
-                updates.append((common_ingts.pop(), common_allgs.pop()))
-
-        # Check certainties
-        for f in unsolved:
-            if len(f[0]) == len(f[1]) == 1:
-                updates.append(tuple(list(x)[0] for x in f))
-
-        # Update found solutions
-        for ingt, allg in updates:
-            solution[ingt] = allg
-            for f in ingt_holders[ingt]:
-                unsolved[f][0].remove(ingt)
-            for f in allg_holders[allg]:
-                unsolved[f][1].remove(allg)
-
-    answer_1 = sum(len(x[0]) for x in unsolved)
-    answer_2 = ",".join(sorted(solution, key=solution.get))
-
-    return answer_1, answer_2
+    _, flow_dict = nx.maximum_flow(G, "source", "sink")
+    solution = {k: max(v, key=v.get) for k, v in flow_dict.items() if k in all_i}
+    return solution
 
 
 if __name__ == "__main__":
     raw_in = read_input("data/day_21.txt")
     foods = parse_inputs(raw_in)
 
-    answer_1, answer_2 = solve(foods)
+    allg_map = max_flow_solve(foods)
+
+    safe_ingts = {k for k, v in allg_map.items() if v == "safe"}
+    allergens = {k: v for k, v in allg_map.items() if v != "safe"}
+
+    answer_1 = sum(1 for igt in safe_ingts for f in foods if igt in f[0])
+    answer_2 = ",".join(sorted(allergens, key=allg_map.get))
 
     print(f"Part 1 answer: {answer_1}")
     print(f"Part 2 answer: {answer_2}")
