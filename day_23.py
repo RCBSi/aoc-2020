a80f74b1c0c554b5d4d823aab336939e819659f2
Jesse Vuorinen <jesse.vuorinen@outlook.com>
2020-12-23 23:33:22 +0200

    separate move from the linked circle class

--- day_23.py
+++ day_23.py
@@ -18,24 +18,21 @@ class LinkedCircle:
 
     def _set_values(self, iterable):
         _iter = iter(iterable)
-        
+
         first_value = next(_iter)
         first_node = CircleNode(first_value)
         self.nodes = {first_value: first_node}
         self.cursor = first_node
 
         for i in _iter:
-            self._push(i)
+            node = CircleNode(i)
+            self.nodes[i] = node
+            self.cursor.next = node
+            self.cursor = node
 
         self.nodes[i].next = first_node  # Wrap to circle
         self.cursor = first_node
         self.max_value = max(self.nodes)
-
-    def _push(self, value):
-        node = CircleNode(value)
-        self.nodes[value] = node
-        self.cursor.next = node
-        self.cursor = node
 
     def peek(self, n):
         res = []
@@ -48,41 +45,38 @@ class LinkedCircle:
     def set_cursor(self, value):
         self.cursor = self.nodes[value]
 
-    def move(self, n):
-        for _ in range(n):
-            self._move()
-
-    def _move(self):
-        peek = self.peek(4)
-        picked_up, next_one = peek[:3], peek[3]
-
-        dst = self.cursor.value - 1
-        if dst == 0:
-            dst = self.max_value
-        while dst in picked_up:
-            dst -= 1
-            if dst == 0:
-                dst = self.max_value
-
-        # Remove the three
-        self.cursor.next = self.nodes[next_one]
-
-        # Attach the picked up ones
-        attach_point = self.nodes[dst].next
-        self.nodes[dst].next = self.nodes[picked_up[0]]
-        self.nodes[picked_up[2]].next = attach_point
-
-        # Update cursor
-        self.cursor = self.cursor.next
-
     def __repr__(self):
         return f"LinkedCircle with {len(self.nodes)} nodes. Cursor is at {self.cursor}"
 
 
+def move(circle, n):
+    for _ in range(n):
+        peek = circle.peek(4)
+        picked_up, skip_to = peek[:3], peek[3]
+
+        dst = circle.cursor.value - 1
+        if dst == 0:
+            dst = circle.max_value
+        while dst in picked_up:
+            dst -= 1
+            if dst == 0:
+                dst = circle.max_value
+
+        # Remove the three
+        circle.cursor.next = circle.nodes[skip_to]
+
+        # Attach the picked up ones
+        attach_point = circle.nodes[dst].next
+        circle.nodes[dst].next = circle.nodes[picked_up[0]]
+        circle.nodes[picked_up[2]].next = attach_point
+
+        # Update cursor
+        circle.cursor = circle.cursor.next
+
 
 def solve_1(inputs):
     cups = LinkedCircle(inputs)
-    cups.move(100)
+    move(cups, 100)
 
     cups.set_cursor(1)
     return "".join(str(i) for i in cups.peek(8))
@@ -93,11 +87,11 @@ def solve_2(inputs):
     _inputs = chain(inputs, range(max(inputs) + 1, MAX_VALUE + 1))
 
     cups = LinkedCircle(_inputs)
-    cups.move(10_000_000)
+    move(cups, 10_000_000)
 
     cups.set_cursor(1)
     a, b = cups.peek(2)
-    return a*b
+    return a * b
 
 
 if __name__ == "__main__":
@@ -105,7 +99,7 @@ if __name__ == "__main__":
     inputs = [int(i) for i in raw_in[0]]
 
     answer_1 = solve_1(inputs)
-    # answer_2 = solve_2(inputs)
+    answer_2 = solve_2(inputs)
 
     print(f"Part 1 answer: {answer_1}")
-    # print(f"Part 2 answer: {answer_2}")
+    print(f"Part 2 answer: {answer_2}")
