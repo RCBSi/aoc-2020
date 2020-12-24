4410631c7a477840bb1cdfae978e9fdc14117f1b
Jesse Vuorinen <jesse.vuorinen@outlook.com>
2020-12-24 10:22:33 +0200

    day 24 done

--- day_24.py
+++ day_24.py
@@ -18,22 +18,46 @@ def parse_instructions(raw_in):
 
 
 def create_floor(instructions):
-    blacks = set()
+    floor = set()
     for path in instructions:
         x, y = (0,0)
         for c in path:
             dx, dy = COORDS[c]
             x += dx
             y += dy
-        if (x, y) in blacks:
-            blacks.remove((x, y))
+        if (x, y) in floor:
+            floor.remove((x, y))
         else:
-            blacks.add((x, y))
-    return blacks
+            floor.add((x, y))
+    return floor
+
+def _get_neighbors(loc):
+    x, y = loc
+    return set((x+dx, y+dy) for dx, dy in COORDS.values())
+    
+
+def _update(floor):
+    new = set()
+
+    for loc in floor:
+        n_neighbors = sum((n in floor) for n in _get_neighbors(loc))
+        if n_neighbors in (1, 2):
+            new.add(loc)
+
+    neighboring = set.union(*(_get_neighbors(x) for x in floor)) - floor
+    for loc in neighboring:
+        n_neighbors = sum((n in floor) for n in _get_neighbors(loc))
+        if n_neighbors == 2:
+            new.add(loc)
+
+    return new
 
 
-def solve_2(raw_in):
-    return False
+
+def solve_2(floor):
+    for _ in range(100):
+        floor = _update(floor)
+    return len(floor)
 
 
 if __name__ == "__main__":
@@ -43,7 +67,7 @@ if __name__ == "__main__":
     floor = create_floor(instructions)
 
     answer_1 = len(floor)
-    answer_2 = solve_2(raw_in)
+    answer_2 = solve_2(floor)
 
     print(f"Part 1 answer: {answer_1}")
     print(f"Part 2 answer: {answer_2}")
